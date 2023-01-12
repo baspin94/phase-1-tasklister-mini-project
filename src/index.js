f4f57030ec5f8d8f148a6ac4b92c850b4512b5f9
Bianca Aspin <bianca@mj94.net>
2023-01-11 23:40:49 -0800

    Added form event listener and built out function to append form submissions to to-do list.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addEventListener("submit", function(e) {
+    //Prevent Reload
+    e.preventDefault();
+    //Callback function
+    buildToDo();
+  })
+  function buildToDo() {
+    //Grab Input
+    let task = document.querySelector("#new-task-description").value;
+    console.log(task);
+    
+    //Create New ToDo Element
+    let newToDo = document.createElement("p");
+    newToDo.textContent = task;
+    console.log(newToDo);
+
+    //Grab List
+    const toDoList = document.querySelector("#list");
+    console.log(toDoList);
+
+    //Append Element to List
+    toDoList.appendChild(newToDo);
+
+    //Reset Form
+    const taskForm = document.querySelector("#create-task-form");
+    taskForm.reset();
+  }
 });
+  
