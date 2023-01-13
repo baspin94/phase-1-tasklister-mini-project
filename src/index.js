dd45a2cedb68e509238583026349789151c2710d
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-01-13 10:05:50 -0700

    added priority selection with corresponding colored texted

--- src/index.js
+++ src/index.js
@@ -6,18 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
     buildToDo();
   })
   function buildToDo() {
-    //Grab Input
+    //Grab Description
     let task = document.querySelector("#new-task-description").value;
-    console.log(task);
-    
+
+    //Grab Priority
+    let priority = document.querySelector('#priority-dropdown').value;
+
     //Create New ToDo Element
     let newToDo = document.createElement("p");
     newToDo.textContent = task;
-    console.log(newToDo);
+    switch (priority) {
+      case '1':
+        newToDo.style.color = 'green';
+        break;
+      case '2':
+        newToDo.style.color = 'yellow';
+        break;
+      case '3':
+        newToDo.style.color = 'red';
+        break;
+    }
 
     //Grab List
     const toDoList = document.querySelector("#list");
-    console.log(toDoList);
 
     //Append Element to List
     toDoList.appendChild(newToDo);
